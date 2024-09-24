400ce5a723786da08536bb984d9cfcdf04aa0ae9
dong <kouyidong99@163.com>
2024-09-24 17:40:16 +0800

    fix: 渲染进程引入全局错误处理器

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import './renderer/styles/dark.css'
 import './renderer/styles/global.css'
 import App from './renderer/App.vue'
 import router from './renderer/router/index.js'
+import { setupGlobalErrorHandler } from './renderer/utils/errorHandler'
 
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+
+// 设置全局错误处理器
+setupGlobalErrorHandler(app)
 
 app.mount('#app')
